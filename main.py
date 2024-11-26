6426318778aa8aec55cfb6c6147bf285c0d499ab
ivysonin <ivysonthauan4@gmail.com>
2024-11-26 12:16:46 -0300

    Added combo display system

---
+++ main.py
@@ -0,0 +1,33 @@
+from combos import combos_economicos, combos_especiais, combos_mcnuggets, combos_tradicionais
+
+# organização
+print ("\n========== Bem vindo ao McDonald's ==========")
+print ("\n          ======= Cardápio =======")
+
+print ("\n========== 1 ==========")
+combos_economicos.McLanche_Feliz()
+print ("\n========== 2 ==========")
+combos_economicos.Combo_Simples()
+print ("\n========== 3 ==========")
+combos_especiais.McCrispy_Chicken_Deluxe()
+print ("\n========== 4 ==========")
+combos_especiais.McFish()
+print ("\n========== 5 ==========")
+combos_especiais.McVeggie()
+print ("\n========== 6 ==========")
+combos_mcnuggets.McNuggets_10_peças()
+print ("\n========== 7 ==========")
+combos_mcnuggets.McNuggets_15_peças()
+print ("\n========== 8 ==========")
+combos_tradicionais.big_Mac()
+print ("\n========== 9 ==========")
+combos_tradicionais.Quarterao_com_queijo()
+print ("\n========== 10 ==========")
+combos_tradicionais.McChicken()
+print ("\n========== 11 ==========")
+combos_tradicionais.Cheddar_McMelt()
+print ("\n========== 12 ==========")
+combos_tradicionais.Duplo_quarterao_com_queijo()
+
+# Lista para adicionar as escolhas do cliente
+lista_usuario = []
