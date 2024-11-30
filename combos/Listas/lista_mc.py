46423be8214aa4292e92041086ca83e2d86110c9
ivysonin <ivysonthauan4@gmail.com>
2024-11-30 18:30:11 -0300

    I created lists to add the combos (I added economic)

---
+++ combos/Listas/lista_mc.py
@@ -0,0 +1,18 @@
+def lista_McLanche_Feliz() :
+    lista = [
+    "===== Combo McLanche Feliz:",
+    "Mini sanduíche Cheeseburger",
+    "Batata frita",
+    "Bebida pequena",
+    "Brinquedo"
+    ]
+    return lista
+
+def lista_Combo_Simples() :
+    lista = [
+    "===== Combo Simples:",
+    "Sanduíches menores McChicken Jr",
+    "Batata frita pequena",
+    "Refrigerante pequeno"
+    ]
+    return lista
