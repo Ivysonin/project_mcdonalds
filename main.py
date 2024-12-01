c8260baf215f353389a533c2c07616adee89706c
ivysonin <ivysonthauan4@gmail.com>
2024-12-01 09:44:04 -0300

    Changed organization

--- main.py
+++ main.py
@@ -6,29 +6,29 @@ print ("\n========== Bem vindo ao McDonald's ==========")
 print ("\n          ======= Cardápio =======")
 
 # Números usados para o cliente fazer suas escolhas mais para frente
-print ("\n========== 1 ==========")
+print ("\n           [ 1 ] \n")
 combos_economicos.McLanche_Feliz()
-print ("\n========== 2 ==========")
+print ("\n           [ 2 ] \n")
 combos_economicos.Combo_Simples()
-print ("\n========== 3 ==========")
+print ("\n           [ 3 ] \n")
 combos_especiais.McCrispy_Chicken_Deluxe()
-print ("\n========== 4 ==========")
+print ("\n           [ 4 ] \n")
 combos_especiais.McFish()
-print ("\n========== 5 ==========")
+print ("\n           [ 5 ] \n")
 combos_especiais.McVeggie()
-print ("\n========== 6 ==========")
+print ("\n           [ 6 ] \n")
 combos_mcnuggets.McNuggets_10_peças()
-print ("\n========== 7 ==========")
+print ("\n           [ 7 ] \n")
 combos_mcnuggets.McNuggets_15_peças()
-print ("\n========== 8 ==========")
+print ("\n           [ 8 ] \n")
 combos_tradicionais.big_Mac()
-print ("\n========== 9 ==========")
+print ("\n           [ 9 ] \n")
 combos_tradicionais.Quarterao_com_queijo()
-print ("\n========== 10 ==========")
+print ("\n           [ 10 ] \n")
 combos_tradicionais.McChicken()
-print ("\n========== 11 ==========")
+print ("\n           [ 11 ] \n")
 combos_tradicionais.Cheddar_McMelt()
-print ("\n========== 12 ==========")
+print ("\n           [ 12 ] \n")
 combos_tradicionais.Duplo_quarterao_com_queijo()
 
 # Lista para adicionar as escolhas do cliente
@@ -38,10 +38,10 @@ while True :
     escolha_usuario = input("\n=== Escolha um número referente ao lanche(Digite 0 para sair): ")
 
     if escolha_usuario == "0" :
-        print ("\n===== Você encerrou os pedidos =====\n ===== Suas escolhas :\n")
+        print ("\n===== Você encerrou os pedidos =====\n           Suas escolhas :\n")
         break
 
     elif escolha_usuario == "1" :
-        print("\n=== McLanche Feliz Adicionado ===\n")
+        print("\n=== McLanche Feliz Adicionado ===")
         # Adicionando os ingredientes dos combos, a lista do usuario.
         lista_usuario.append(lista_mc.lista_McLanche_Feliz())
