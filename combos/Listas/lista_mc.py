6bb828e4f2690a2a534477fb3967f9cd25e3f5d3
ivysonin <ivysonthauan4@gmail.com>
2024-11-30 22:20:20 -0300

    Adding lists of remaining combos

--- combos/Listas/lista_mc.py
+++ combos/Listas/lista_mc.py
@@ -1,3 +1,4 @@
+# Combos economicos
 def lista_McLanche_Feliz() :
     lista = [
     "===== Combo McLanche Feliz:",
@@ -14,5 +15,98 @@ def lista_Combo_Simples() :
     "Sanduíches menores McChicken Jr",
     "Batata frita pequena",
     "Refrigerante pequeno"
+    ]
+    return lista
+
+# Combos especiais
+def lista_McCrispy_Chicken_Deluxe() :
+    lista = [
+    "===== Combo McCrispy Chicken Deluxe:",
+    "Sanduíche com frango empanado crocante, alface e molho especial",
+    "Batata frita média",
+    "Refrigerante"
+    ]
+    return lista
+
+def lista_McFish() :
+    lista = [
+    "===== Combo McFish:",
+    "Sanduíche com filé de peixe empanado e molho tártaro",
+    "Batata frita média",
+    "Refrigerante"
+    ]
+    return lista
+
+def lista_McVeggie() :
+    lista = [
+    "===== Combo McVeggie:",
+    "Sanduíche vegetariano com hambúrguer à base de vegetais",
+    "Batata frita média",
+    "Refrigerante"
+    ]
+    return lista
+
+# Combos mcnuggets
+def lista_McNuggets_10_peças() :
+    lista = [
+    "===== Combo McNuggets 10 peças:",
+    "10 unidades de McNuggets",
+    "Batata frita média",
+    "Refrigerante"
+    ]
+    return lista
+
+def lista_McNuggets_15_peças() :
+    lista = [
+    "===== Combo McNuggets 15 peças:",
+    "15 unidades de McNuggets",
+    "Batata frita média",
+    "Refrigerante"
+    ]
+    return lista
+
+# Combos tradicionais
+def lista_big_Mac() :
+    lista = [
+    "===== Big Mac:",
+    "Sanduíche Big Mac",
+    "Batata frita média",
+    "Refrigerante"
+    ]
+    return lista
+
+def lista_Quarterao_com_queijo() :
+    lista = [
+    "===== Quarterão com Queijo:",
+    "Sanduíche Quarterão com Queijo",
+    "Batata frita média",
+    "Refrigerante"
+    ]
+    return lista
+
+def lista_McChicken() :
+    lista = [
+    "===== McChicken:",
+    "Sanduíche McChicken",
+    "Batata frita média",
+    "Refrigerante"
+    ]
+    return lista
+
+def lista_Cheddar_McMelt() :
+    lista = [
+    "===== Cheddar McMelt:",
+    "Sanduíche Cheddar McMelt",
+    "Batata frita média",
+    "Refrigerante"
+    ]
+    return lista
+
+def lista_Duplo_quarterao_com_queijo() :
+    lista = [
+    "===== Duplo Quarterão com Queijo:",
+    "Sanduíche Duplo Quarterão",
+    "Batata frita média",
+    "Refrigerante"
     ]
     return lista
