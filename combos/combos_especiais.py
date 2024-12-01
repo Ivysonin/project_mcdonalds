4e9b9a45b99f4b7273ef160a3605b17dcd883127
ivysonin <ivysonthauan4@gmail.com>
2024-11-30 22:22:27 -0300

    Changed display look(str)

--- combos/combos_especiais.py
+++ combos/combos_especiais.py
@@ -6,12 +6,12 @@ def McCrispy_Chicken_Deluxe() :
 
 def McFish() :
     print ("===== McFish =====\n")
-    print ("1 - Sanduíche com filé de peixe empanado e molho tártaro")
-    print ("2 - Batata frita média")
-    print ("3 - Refrigerante")
+    print ("- Sanduíche com filé de peixe empanado e molho tártaro")
+    print ("- Batata frita média")
+    print ("- Refrigerante")
 
 def McVeggie() :
     print ("===== McVeggie =====\n")
-    print ("1 - Sanduíche vegetariano com hambúrguer à base de vegetais")
-    print ("2 - Batata frita média")
-    print ("3 - Refrigerante ou suco")
+    print ("- Sanduíche vegetariano com hambúrguer à base de vegetais")
+    print ("- Batata frita média")
+    print ("- Refrigerante")
